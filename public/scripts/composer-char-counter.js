2deedce1732786da93c913bcc65f5284c59aa310
Jashan <jashan_brar50@hotmail.com>
2019-12-17 21:47:08 +0000

    joined character counter span to text area input

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,10 +1,15 @@
 $(document).ready(function() {
   console.log('working')
-  // $("#text-area").keyup(function () {
-  //   console.log()
-  // })
   $("#text-area").on("keyup", function () {
-    let length = $("#text-area").val().length;
-    console.log(length);
+    let length = $(this).val().length;
+    console.log(140 - length);
+    console.log($(this).parent())
+    if (length <= 140) {
+      $(this).parent().children("span").text((140 - length));
+    } else {
+      $(this).parent().children("span").text(0);
+      $(this).parent().children("span").css("background-color", "red");
+    }
   })
+
 });
