4f8cc5a11233b005238bda113334cdaaee2aa3c2
Jashan <jashan_brar50@hotmail.com>
2019-12-17 20:35:06 +0000

    added count length of tweet field after keyup

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -4,6 +4,7 @@ $(document).ready(function() {
   //   console.log()
   // })
   $("#text-area").on("keyup", function () {
-    let length = $("#text-area").
+    let length = $("#text-area").val().length;
+    console.log(length);
   })
 });
