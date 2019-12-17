d64da9eb500e53d4a4342ff4d317c222bd1077dd
Jashan <jashan_brar50@hotmail.com>
2019-12-17 20:26:41 +0000

    First commit-have header, avatar image/name and compose tweet form

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  console.log('working')
+  // $("#text-area").keyup(function () {
+  //   console.log()
+  // })
+  $("#text-area").on("keyup", function () {
+    let length = $("#text-area").
+  })
+});
