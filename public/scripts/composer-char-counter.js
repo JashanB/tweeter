448beecab7a28711795b2215e033142af96339a8
Jashan <jashan_brar50@hotmail.com>
2019-12-18 00:04:20 +0000

    added tweets container and styled

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,9 +1,7 @@
 $(document).ready(function() {
-  console.log('working')
   $("#text-area").on("keyup", function () {
     let length = $(this).val().length;
     $(this).parent().children("span").text((140 - length));
-    console.log($(this).parent())
     if (length <= 140) {
       $(this).parent().children("span").css("background-color", "#f4f1ec");
     } else {
