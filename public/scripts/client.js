0183907ec77adc7408b651dd8e2e3e6efcce5307
Jashan <jashan_brar50@hotmail.com>
2019-12-19 19:40:10 +0000

    add accurate time function, add on hover functions

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,16 +1,49 @@
 //creates tweet element and appends to section tweet container
 const createTweetElement = function(tweet) {
   //create escape element for tweet content div
-  const escape = function(str) {
+  const escape = function (str) {
     let div = document.createElement('div');
     div.appendChild(document.createTextNode(str));
     return div.innerHTML;
   }
-  const $tweet = $('<article>').addClass('tweet');
-  let date = Math.floor((Date.now() - tweet.created_at) / 86400000); //converts miliseconds to days
-  if (date < 0) {
-    date = 0;
+  //return time accurately
+  const currentTime = function(time) {
+    let reportedTime = 0;
+    if (Math.floor((Date.now() - time) / 86400000) > 0) {
+      reportedTime = Math.floor((Date.now() - time) / 86400000);
+      if (reportedTime === 1) {
+        return reportedTime + ' day ago';
+      } else {
+        return reportedTime + ' days ago';
+      }
+    } else if (Math.floor((Date.now() - time) / 3600000) > 0) {
+      reportedTime = Math.floor((Date.now() - time) / 3600000);
+      if (reportedTime === 1) {
+        return reportedTime + ' hour ago';
+      } else {
+        return reportedTime + ' hours ago';
+      }
+    } else if (Math.floor((Date.now() - time) / 60000) > 0) {
+      reportedTime = Math.floor((Date.now() - time) / 60000);
+      if (reportedTime === 1) {
+        return reportedTime + ' minute ago';
+      } else {
+        return reportedTime + ' minutes ago';
+      }
+    } else {
+      reportedTime = Math.floor((Date.now() - time) / 1000);
+      if (reportedTime < 0) {
+        reportedTime = 0;
+      }
+      if (reportedTime === 1) {
+        return reportedTime + ' second ago';
+      } else {
+        return reportedTime + ' seconds ago';
+      }
+    }
   }
+  //creates tweet article for each tweet
+  const $tweet = $('<article>').addClass('tweet');
   const htmlCode = `
   <header>
     <span class="display-name">${tweet.user.name}</span>
@@ -18,7 +51,7 @@ const createTweetElement = function(tweet) {
     <img src=${tweet.user.avatars}/>
   </header>
   <div>${escape(tweet.content.text)}</div>
-  <footer>${date} days ago<img src="/images/twitter-share.png"></footer>
+  <footer>${currentTime(tweet.created_at)}<img class="share" src="/images/twitter-share.png"></footer>
   `
   $tweet.append(htmlCode);
   return $tweet;
@@ -30,7 +63,7 @@ const renderTweets = function(tweets) {
   })
 }
 
-$(document).ready(function() {
+$(document).ready(function () {
   //submit tweet to /tweets
   $('#compose-tweet').submit(function () {
     event.preventDefault();
@@ -43,18 +76,21 @@ $(document).ready(function() {
       $('#error').text('Character limit exceeded');
     } else {
       $('#error').hide();
-      $.post({type: "POST", url: '/tweets', data: $('#compose-tweet').serialize(), success: () => {
-        loadTweets(); //loads new tweets
-      }})  
+      $.post({
+        type: "POST", url: '/tweets', data: $('#compose-tweet').serialize(), success: () => {
+          loadTweets(); //loads new tweets
+        }
+      })
       $("#compose-tweet").trigger('reset');
+      $('.counter').text('140');
     }
   })
   //load tweets posted to /tweets
-  const loadTweets = function() {
-    $.ajax('/tweets', {method: 'GET'})
-    .then(function (tweets) {
-      renderTweets(tweets);
-    })
+  const loadTweets = function () {
+    $.ajax('/tweets', { method: 'GET' })
+      .then(function (tweets) {
+        renderTweets(tweets);
+      })
   }
   //loads current tweets in history
   loadTweets();
--- public/scripts/hide.js
+++ public/scripts/hide.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+  //hides new tweet box until click-toggles it
   $("#new-tweet").hide(200);
   $("#write-new-tweet").on('click', function () {
     $("#new-tweet").slideToggle(function() {
